fc35293ef4d1c78edf66718f0413a9960b5943a5
Lalit <lalitkumarsingh3716@gmail.com>
2022-12-10 02:42:29 +0530

    feat: Connected MongoDB 🚀

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,14 @@
 import express from "express";
-import { sayHello } from "@utils/sayHello";
+import { PORT } from "@config/keys";
+import connectDB from "@config/db";
 
 const app = express();
+connectDB();
 
-app.get("/", async (req, res) => {
+app.get("/", async (_req, res) => {
   res.send("Hello World");
 });
 
-sayHello("World");
-
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} :)`);
 });
--- server/src/utils/sayHello.ts
+++
@@ -1,3 +0,0 @@
-export const sayHello = (name: string) => {
-  console.log(`Hello ${name}!`);
-};
