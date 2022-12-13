6c2ea7b76b3e33a49e2f21d96baaf05cdadd2d56
Lalit <lalitkumarsingh3716@gmail.com>
2022-12-13 14:56:48 +0530

    feat: Better api response schema

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -5,7 +5,14 @@ import { Request, Response } from "express";
 export const registerOrg = async (req: Request, res: Response) => {
   const org = new Org(req.body);
   await org.save();
-  res.status(201).json(org);
+
+  const token = org.issueToken();
+  res.cookie(AUTH_COOKIE.name, token, AUTH_COOKIE.options);
+  res.status(201).json({
+    success: true,
+    message: "Org created successfully",
+    data: { id: org._id }
+  });
 };
 
 export const loginOrg = async (req: Request, res: Response) => {
@@ -13,15 +20,35 @@ export const loginOrg = async (req: Request, res: Response) => {
   const org = await Org.findOne({ name });
 
   if (!org) {
-    return res.status(404).json({ message: "Org not found" });
+    return res.status(404).json({
+      success: false,
+      message: "Org not found",
+      data: { name }
+    });
   }
 
   const isMatch = await org.verifyPassword(password);
 
   if (!isMatch) {
-    return res.status(401).json({ error: "Invalid credentials" });
+    return res.status(401).json({
+      success: false,
+      message: "Invalid credentials",
+      data: {
+        error: "Invalid credentials"
+      }
+    });
   }
 
   res.cookie(AUTH_COOKIE.name, org.issueToken(), AUTH_COOKIE.options);
-  res.status(200).json(org);
+  res.status(200).json({
+    success: true,
+    message: "Org logged in successfully",
+    data: {
+      id: org._id,
+      name: org.name,
+      description: org.description,
+      email: org.email,
+      website: org.website
+    }
+  });
 };
